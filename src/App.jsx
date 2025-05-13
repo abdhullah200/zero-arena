ff661bbb9142f4ba65bbf1e88bc8bda30a0725ec
Abdhullah Ariff <139129030+abdhullah200@users.noreply.github.com>
2025-05-13 23:00:15 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import Header from './components/Header.jsx';
-import Hero from "./components/Hero.jsx";
-import Characters from "./components/Characters.jsx";
-import Arena from "./components/Arena.jsx";
-import Footer from "./components/Footer.jsx";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Characters from "./components/Characters";
+import Arena from "./components/Arena";
+import Footer from "./components/Footer";
+import SkeletonHero from "./components/SkeletonHero";
+import SkeletonSection from "./components/SkeletonSection";
 
 const App = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoading(false);
+        }, 2000); // Simulate loading
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
-        <div className=''>
-            <Header/>
-            <Hero/>
-            <Characters/>
-            <Arena/>
-            <Footer/>
+        <div>
+            <Header />
+            <div id="hero">{loading ? <SkeletonHero /> : <Hero />}</div>
+            <div id="Characters">{loading ? <SkeletonSection /> : <Characters />}</div>
+            <div id="arena">{loading ? <SkeletonSection /> : <Arena />}</div>
+            <Footer />
         </div>
-    )
-}
-export default App
+    );
+};
+
+export default App;
