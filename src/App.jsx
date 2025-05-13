30ab46cc49cbec6fa8888dfb5ebd1bc431c990df
Abdhullah Ariff <139129030+abdhullah200@users.noreply.github.com>
2025-05-13 23:15:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,47 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Characters from "./components/Characters";
 import Arena from "./components/Arena";
 import Footer from "./components/Footer";
-import SkeletonHero from "./components/SkeletonHero";
-import SkeletonSection from "./components/SkeletonSection";
+import LoadingScreen from "./components/LoadingScreen";
 
 const App = () => {
     const [loading, setLoading] = useState(true);
+    const videoRef = useRef(null);
 
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setLoading(false);
-        }, 2000); // Simulate loading
-        return () => clearTimeout(timer);
+        const handleLoaded = () => {
+            // Simulate delay to feel like it's loading assets
+            setTimeout(() => setLoading(false), 1000);
+        };
+
+        const video = videoRef.current;
+        if (video && video.readyState >= 3) {
+            handleLoaded();
+        } else {
+            video?.addEventListener("canplaythrough", handleLoaded);
+        }
+
+        return () => video?.removeEventListener("canplaythrough", handleLoaded);
     }, []);
 
     return (
-        <div>
-            <Header />
-            <div id="hero">{loading ? <SkeletonHero /> : <Hero />}</div>
-            <div id="Characters">{loading ? <SkeletonSection /> : <Characters />}</div>
-            <div id="arena">{loading ? <SkeletonSection /> : <Arena />}</div>
-            <Footer />
+        <div className="relative">
+            {loading && <LoadingScreen />}
+            <div className={`${loading ? "invisible" : "visible"} transition-opacity duration-1000`}>
+                <Header />
+                <div id="hero">
+                    <Hero videoRef={videoRef} />
+                </div>
+                <div id="Characters">
+                    <Characters />
+                </div>
+                <div id="arena">
+                    <Arena />
+                </div>
+                <Footer />
+            </div>
         </div>
     );
 };
